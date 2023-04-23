97dc4e38317554c15ceecad25fcd669f86a74129
Pravin Bhagwat <85389806+pravinbhagwat@users.noreply.github.com>
2023-04-23 16:40:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+ const enterBtn = document.getElementById("enterBtn");
+      const status = document.getElementById("status");
+
+      enterBtn.addEventListener("click", () => {
+        status.innerHTML = "<h1>Entered Metaverse</h1>";
+      });
